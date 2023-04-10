85070b47ff0a2a5d7911351e450548b6972cbce6
Dung Phan <fshu.pbdn@gmail.com>
2023-04-10 22:33:59 +0700

    first commit

---
+++ input.js
@@ -0,0 +1,53 @@
+// write 3 console log that print "Hello", "Hi", "Welcome"
+// console.log("Hello");
+// console.log("Hi");
+// console.log("Welcome");
+// console.log("");
+// console.log("");
+// console.log("");
+
+// Abc, Bcd, Cde, Def, Efggg
+// const student = [
+//   { firstname: "Abc", score: 100 },
+//   { firstname: "Bcd", score: 100 },
+//   { firstname: "Cde", score: 100 },
+//   { firstname: "Def", score: 100 },
+//   { firstname: "Efggg", score: 100 },
+// ];
+
+// let s = "hello";
+// console.log(s[0]);
+// s[0] = "C";
+// console.log(s);
+// s = "annyeonghaseyo";
+// console.log(s);
+
+// function mul(a, b) {
+//   return a * b;
+// }
+
+// const multi = (a, b) => a * b;
+
+// // console.log(mul(5, 6));
+// // console.log(multi(2, 8));
+
+// // function square(x) {
+// //   return mul(x, x);
+// // }
+// const square = (x) => multi(x, x);
+
+// function run(fn) {
+//   console.log(`running ${fn}`);
+//   console.log(`result: ${fn(10)}`);
+//   console.log("done");
+// }
+
+// run(square);
+
+// //no argument
+// const greeting = () => console.log("hi");
+// console.log(greeting());
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+ReactDOM.render(<p>Hello</p>, document.getElementById("root"));
